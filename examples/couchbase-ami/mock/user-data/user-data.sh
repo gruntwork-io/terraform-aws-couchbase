1a65f8111a361e68bb285744af37f593a48f0bd5
Yevgeniy Brikman <brikis98@gmail.com>
2018-03-21 13:41:53 +0000

    Fill in sync gateway run script

--- examples/couchbase-ami/mock/user-data/user-data.sh
+++ examples/couchbase-ami/mock/user-data/user-data.sh
@@ -8,5 +8,16 @@ exec > >(tee /opt/couchbase/var/lib/couchbase/logs/mock-user-data.log|logger -t 
 
 # The variables below come from mock-couchbase.env or entrypoint.env
 /opt/couchbase/bin/run-couchbase-server \
+  --cluster-name "${cluster_asg_name}" \
   --cluster-username "${cluster_username}" \
-  --cluster-password "${cluster_password}"
+  --cluster-password "${cluster_password}" \
+  --rally-point-port "${cluster_port}" \
+  --rest-port "${cluster_port}"
+
+/opt/couchbase-sync-gateway/bin/run-sync-gateway \
+  --auto-fill-asg "<SERVERS>=${cluster_asg_name}:${cluster_port}" \
+  --auto-fill "<INTERFACE>=${sync_gateway_interface}" \
+  --auto-fill "<ADMIN_INTERFACE>=${sync_gateway_admin_interface}" \
+  --auto-fill "<DB_NAME>=${cluster_asg_name}" \
+  --auto-fill "<DB_USERNAME>=${cluster_username}" \
+  --auto-fill "<DB_PASSWORD>=${cluster_password}"
