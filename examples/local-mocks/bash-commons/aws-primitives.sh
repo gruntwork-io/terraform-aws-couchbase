3cd586ce7d93eb5d076a2b16b1fb51ff7b5e9bb3
Yevgeniy Brikman <brikis98@gmail.com>
2018-03-28 14:20:11 +0100

    Make hostname configurable

--- examples/local-mocks/bash-commons/aws-primitives.sh
+++ examples/local-mocks/bash-commons/aws-primitives.sh
@@ -22,12 +22,12 @@ function get_instance_public_hostname {
 }
 
 function get_instance_region {
-  # This variable is set in local-mocks/entrypoint/entrypoint.sh
+  # This variable is set in docker-compose.yml
   echo "$mock_aws_region"
 }
 
 function get_ec2_instance_availability_zone {
-  # This variable is set in local-mocks/entrypoint/entrypoint.sh
+  # This variable is set in docker-compose.yml
   echo "$mock_availability_zone"
 }
 
@@ -41,7 +41,7 @@ function get_instance_tags {
   local readonly instance_id="$1"
   local readonly instance_region="$2"
 
-  # The cluster_asg_name below is an env var from local-mocks/entrypoint/entrypoint.sh
+  # The cluster_asg_name below is an env var from docker-compose.yml
   cat << EOF
 {
   "Tags": [
@@ -67,12 +67,13 @@ function describe_asg {
   local readonly asg_name="$1"
   local readonly aws_region="$2"
 
+  # The cluster_size below is an env var from docker-compose.yml
   cat << EOF
 {
   "AutoScalingGroups": [
     {
       "AutoScalingGroupARN": "arn:aws:autoscaling:$aws_region:123456789012:autoScalingGroup:930d940e-891e-4781-a11a-7b0acd480f03:autoScalingGroupName/$asg_name",
-      "DesiredCapacity": 3,
+      "DesiredCapacity": $cluster_size,
       "AutoScalingGroupName": "$asg_name",
       "LaunchConfigurationName": "$asg_name",
       "CreatedTime": "2013-08-19T20:53:25.584Z"
@@ -90,11 +91,12 @@ function describe_instances_in_asg {
   # These hostnames are set by Docker Compose networking using the names of the services
   # (https://docs.docker.com/compose/networking/). We use getent (https://unix.stackexchange.com/a/20793/215969) to get
   # the IP addresses for these hostnames, as that's what the servers themselves will advertise (see the mock
-  # get_instance_xxx_hostname methods above).
+  # get_instance_xxx_hostname methods above). Note that the data_node_container_base_name variable below is set in
+  # docker-compose.yml.
 
-  local readonly couchbase_hostname_0=$(getent hosts couchbase-ubuntu-0 | awk '{ print $1 }')
-  local readonly couchbase_hostname_1=$(getent hosts couchbase-ubuntu-1 | awk '{ print $1 }')
-  local readonly couchbase_hostname_2=$(getent hosts couchbase-ubuntu-2 | awk '{ print $1 }')
+  local readonly couchbase_hostname_0=$(getent hosts "$data_node_container_base_name-0" | awk '{ print $1 }')
+  local readonly couchbase_hostname_1=$(getent hosts "$data_node_container_base_name-1" | awk '{ print $1 }')
+  local readonly couchbase_hostname_2=$(getent hosts "$data_node_container_base_name-2" | awk '{ print $1 }')
 
   cat << EOF
 {
