f563bcd471d174aa337bc366d453bda7fab30872
Yevgeniy Brikman <brikis98@gmail.com>
2018-03-31 01:00:57 +0100

    Fix Sync Gateway cluster name in multi cluster test

--- test/couchbase_multi_cluster_test.go
+++ test/couchbase_multi_cluster_test.go
@@ -59,7 +59,7 @@ func testCouchbaseMultiCluster(t *testing.T, testName string, osName string) {
 
 	test_structure.RunTestStage("validation", logger, func() {
 		terratestOptions := test_structure.LoadTerratestOptions(t, couchbaseMultiClusterDir, logger)
-		clusterName := getClusterName(t, syncGatewayClusterVarName, terratestOptions)
+		clusterName := getClusterName(t, dataNodeClusterVarName, terratestOptions)
 
 		couchbaseDataNodesUrl, err := terratest.OutputRequired(terratestOptions, "couchbase_data_nodes_web_console_url")
 		if err != nil {
