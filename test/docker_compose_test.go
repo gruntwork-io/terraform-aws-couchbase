ca981da738f0e87f88825b6a8af49b602877d730
Yevgeniy Brikman <brikis98@gmail.com>
2018-04-02 15:51:11 +0100

    Try reducing the number of parallel Docker builds

--- test/docker_compose_test.go
+++ test/docker_compose_test.go
@@ -10,29 +10,59 @@ import (
 	"github.com/gruntwork-io/terratest/shell"
 	"strconv"
 	"github.com/gruntwork-io/terratest/util"
+	"sync"
 )
 
-func TestUnitCouchbaseSingleClusterUbuntuInDocker(t *testing.T) {
-	t.Parallel()
-	testCouchbaseInDocker(t, "TestUnitCouchbaseSingleClusterUbuntuInDocker","couchbase-single-cluster", "ubuntu", 2, 8091, 4984)
+// This type is used to ensure that a given Docker build runs at most once. Repated calls to the Build() method will
+// not rebuild the same Docker image.
+type DockerBuilder sync.Once
+
+func (build DockerBuilder) Build(t *testing.T, osName string, logger *log.Logger, packerTemplateDir string) {
+	asOnce := sync.Once(build)
+	(&asOnce).Do(func() {
+		buildCouchbaseWithPacker(t, logger, fmt.Sprintf("%s-docker", osName), "couchbase","us-east-1", packerTemplateDir)
+	})
 }
 
-func TestUnitCouchbaseMultiClusterUbuntuInDocker(t *testing.T) {
-	t.Parallel()
-	testCouchbaseInDocker(t, "TestUnitCouchbaseMultiClusterUbuntuInDocker", "couchbase-multi-cluster","ubuntu", 3,7091, 3984)
+func TestUnitCouchbaseInDocker(t *testing.T) {
+	testCases := []struct {
+		testName string
+		examplesFolderName string
+		osName string
+		clusterSize int
+		couchbaseWebConsolePort int
+		syncGatewayWebConsolePort int
+	} {
+		{"TestUnitCouchbaseSingleClusterUbuntuInDocker","couchbase-single-cluster", "ubuntu", 2, 8091, 4984},
+		{"TestUnitCouchbaseMultiClusterUbuntuInDocker", "couchbase-multi-cluster","ubuntu", 3,7091, 3984},
+		{"TestUnitCouchbaseSingleClusterAmazonLinuxInDocker","couchbase-single-cluster", "amazon-linux", 2, 6091, 2984},
+		{"TestUnitCouchbaseMultiClusterAmazonLinuxInDocker", "couchbase-multi-cluster","amazon-linux", 3,5091, 1984},
+	}
+
+	// For some reason, if we run too many Docker builds in parallel, they start to fail with strange, intermittent
+	// errors:
+	//
+	// "Failed to upload to 'XXX' in container: Error response from daemon: Error processing tar file(exit status 1): lchown YYY: no such file or directory
+	//
+	// We only need to build the container for each OS once per build, so we use the DockerBuilder struct to ensure
+	// each test runs the build for a given OS at most once.
+	dockerBuilders := map[string]DockerBuilder{}
+
+	for _, testCase := range testCases {
+		dockerBuilder, containsBuilderForOs := dockerBuilders[testCase.osName]
+		if !containsBuilderForOs {
+			dockerBuilder = DockerBuilder{}
+			dockerBuilders[testCase.osName] = dockerBuilder
+		}
+
+		t.Run(testCase.testName, func(t *testing.T) {
+			t.Parallel()
+			testCouchbaseInDocker(t, testCase.testName, testCase.examplesFolderName, testCase.osName, testCase.clusterSize, testCase.couchbaseWebConsolePort, testCase.syncGatewayWebConsolePort, dockerBuilder)
+		})
+	}
 }
 
-func TestUnitCouchbaseSingleClusterAmazonLinuxInDocker(t *testing.T) {
-	t.Parallel()
-	testCouchbaseInDocker(t, "TestUnitCouchbaseSingleClusterAmazonLinuxInDocker","couchbase-single-cluster", "amazon-linux", 2, 6091, 2984)
-}
-
-func TestUnitCouchbaseMultiClusterAmazonLinuxInDocker(t *testing.T) {
-	t.Parallel()
-	testCouchbaseInDocker(t, "TestUnitCouchbaseMultiClusterAmazonLinuxInDocker", "couchbase-multi-cluster","amazon-linux", 3,5091, 1984)
-}
-
-func testCouchbaseInDocker(t *testing.T, testName string, examplesFolderName string, osName string, clusterSize int, couchbaseWebConsolePort int, syncGatewayWebConsolePort int) {
+func testCouchbaseInDocker(t *testing.T, testName string, examplesFolderName string, osName string, clusterSize int, couchbaseWebConsolePort int, syncGatewayWebConsolePort int, dockerBuilder DockerBuilder) {
 	logger := terralog.NewLogger(testName)
 
 	tmpExamplesDir := test_structure.CopyTerraformFolderToTemp(t, "../", "examples", testName, logger)
@@ -41,7 +71,7 @@ func testCouchbaseInDocker(t *testing.T, testName string, examplesFolderName str
 	uniqueId := util.UniqueId()
 
 	test_structure.RunTestStage("setup_image", logger, func() {
-		buildCouchbaseWithPacker(t, logger, fmt.Sprintf("%s-docker", osName), "couchbase","us-east-1", couchbaseAmiDir)
+		dockerBuilder.Build(t, osName, logger, couchbaseAmiDir)
 	})
 
 	test_structure.RunTestStage("setup_docker", logger, func() {
