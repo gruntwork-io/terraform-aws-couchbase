e180ab8038664a116674845a57cf29fc318534f2
Yevgeniy Brikman <brikis98@gmail.com>
2019-06-24 17:15:12 -0400

    Update couchbase-iam-policies to 0.12

--- modules/couchbase-iam-policies/main.tf
+++ modules/couchbase-iam-policies/main.tf
@@ -1,11 +1,20 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 0.12"
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # ATTACH AN IAM POLICY THAT ALLOWS THE COUCHBASE NODES TO AUTOMATICALLY DISCOVER EACH OTHER AND FORM A CLUSTER
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_iam_role_policy" "auto_discover_cluster" {
   name   = "auto-discover-cluster"
-  role   = "${var.iam_role_id}"
-  policy = "${data.aws_iam_policy_document.auto_discover_cluster.json}"
+  role   = var.iam_role_id
+  policy = data.aws_iam_policy_document.auto_discover_cluster.json
 }
 
 data "aws_iam_policy_document" "auto_discover_cluster" {
@@ -21,3 +30,4 @@ data "aws_iam_policy_document" "auto_discover_cluster" {
     resources = ["*"]
   }
 }
+
--- modules/couchbase-iam-policies/variables.tf
+++ modules/couchbase-iam-policies/variables.tf
@@ -5,4 +5,6 @@
 
 variable "iam_role_id" {
   description = "The ID of the IAM Role to which these IAM policies should be attached"
+  type        = string
 }
+
