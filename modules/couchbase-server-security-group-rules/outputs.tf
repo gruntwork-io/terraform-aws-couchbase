efbb30db5b93738e765758bf026566983b6c588e
Yevgeniy Brikman <brikis98@gmail.com>
2019-06-24 17:14:33 -0400

    Update couchbase-server-security-group-rules

--- modules/couchbase-server-security-group-rules/outputs.tf
+++ modules/couchbase-server-security-group-rules/outputs.tf
@@ -1,71 +1,72 @@
 output "rest_port" {
-  value = "${var.rest_port}"
+  value = var.rest_port
 }
 
 output "ssl_rest_port" {
-  value = "${var.ssl_rest_port}"
+  value = var.ssl_rest_port
 }
 
 output "capi_port" {
-  value = "${var.capi_port}"
+  value = var.capi_port
 }
 
 output "ssl_capi_port" {
-  value = "${var.ssl_capi_port}"
+  value = var.ssl_capi_port
 }
 
 output "query_port" {
-  value = "${var.query_port}"
+  value = var.query_port
 }
 
 output "ssl_query_port" {
-  value = "${var.ssl_query_port}"
+  value = var.ssl_query_port
 }
 
 output "fts_port" {
-  value = "${var.fts_port}"
+  value = var.fts_port
 }
 
 output "ssl_fts_port" {
-  value = "${var.ssl_fts_port}"
+  value = var.ssl_fts_port
 }
 
 output "memcached_port" {
-  value = "${var.memcached_port}"
+  value = var.memcached_port
 }
 
 output "ssl_memcached_port" {
-  value = "${var.ssl_memcached_port}"
+  value = var.ssl_memcached_port
 }
 
 output "moxi_port" {
-  value = "${var.moxi_port}"
+  value = var.moxi_port
 }
 
 output "epmd_port" {
-  value = "${var.epmd_port}"
+  value = var.epmd_port
 }
 
 output "indexer_start_port_range" {
-  value = "${var.indexer_start_port_range}"
+  value = var.indexer_start_port_range
 }
 
 output "indexer_end_port_range" {
-  value = "${var.indexer_end_port_range}"
+  value = var.indexer_end_port_range
 }
 
 output "projector_port" {
-  value = "${var.projector_port}"
+  value = var.projector_port
 }
 
 output "memcached_dedicated_port" {
-  value = "${var.memcached_dedicated_port}"
+  value = var.memcached_dedicated_port
 }
 
 output "internal_data_start_port_range" {
-  value = "${var.internal_data_start_port_range}"
+  value = var.internal_data_start_port_range
 }
 
 output "internal_data_end_port_range" {
-  value = "${var.internal_data_end_port_range}"
+  value = var.internal_data_end_port_range
 }
+
