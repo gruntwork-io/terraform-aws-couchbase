85bbd88ebf982b0b7579262f0b565a02f13c0c5a
Yevgeniy Brikman <brikis98@gmail.com>
2019-06-24 16:50:32 -0400

    Update sync-gateway-security-group-rules to 0.12

--- modules/sync-gateway-security-group-rules/variables.tf
+++ modules/sync-gateway-security-group-rules/variables.tf
@@ -5,6 +5,7 @@
 
 variable "security_group_id" {
   description = "The ID of the Security Group to which all the rules should be attached."
+  type        = string
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -14,44 +15,49 @@ variable "security_group_id" {
 
 variable "interface_port" {
   description = "The port to use for the main Sync Gateway REST interface."
+  type        = number
   default     = 4984
 }
 
 variable "interface_port_cidr_blocks" {
   description = "The list of IP address ranges in CIDR notation from which to allow connections to the interface_port."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "interface_port_security_groups" {
   description = "The list of Security Group IDs from which to allow connections to the interface_port. If you update this variable, make sure to update var.num_interface_port_security_groups too!"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "num_interface_port_security_groups" {
   description = "The number of security group IDs in var.interface_port_security_groups. We should be able to compute this automatically, but due to a Terraform limitation, if there are any dynamic resources in var.allow_inbound_from_cidr_blocks, then we won't be able to: https://github.com/hashicorp/terraform/pull/11482"
+  type        = number
   default     = 0
 }
 
 variable "admin_interface_port" {
   description = "The port to use for the Sync Gateway Admin interface."
+  type        = number
   default     = 4985
 }
 
 variable "admin_interface_port_cidr_blocks" {
   description = "The list of IP address ranges in CIDR notation from which to allow connections to the admin_interface_port. The admin interface exposes ALL Couchbase data, so you probably want to leave this list empty and only allow access from localhost!"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "admin_interface_port_security_groups" {
   description = "The list of Security Group IDs from which to allow connections to the admin_interface_port. The admin interface exposes ALL Couchbase data, so you probably want to leave this list empty and only allow access from localhost! If you update this variable, make sure to update var.num_admin_interface_port_security_groups too!"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "num_admin_interface_port_security_groups" {
   description = "The number of security group IDs in var.admin_interface_port_security_groups. We should be able to compute this automatically, but due to a Terraform limitation, if there are any dynamic resources in var.allow_inbound_from_cidr_blocks, then we won't be able to: https://github.com/hashicorp/terraform/pull/11482"
+  type        = number
   default     = 0
 }
+
