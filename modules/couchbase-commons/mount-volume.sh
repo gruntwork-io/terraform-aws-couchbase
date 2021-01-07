dc7bb7937a12241057f262e42c8a899674398c85
Jake Mallory <4095+tinomen@users.noreply.github.com>
2021-01-07 09:11:16 -0700

    fix issue with finding device, use the path 🤦‍♂️

--- modules/couchbase-commons/mount-volume.sh
+++ modules/couchbase-commons/mount-volume.sh
@@ -8,34 +8,35 @@ source "/opt/gruntwork/bash-commons/log.sh"
 # This method is used to configure a new EBS volume. It formats the specified device name using ext4 and mounts it at
 # the given mount point, with the given OS user as owner.
 function mount_volume {
-  local device_name="$1"
+  local readonly device_name="$1"
   local readonly mount_point="$2"
   local readonly ebs_size="$3"
   local readonly owner="$4"
   local readonly file_system_type="${5:-ext4}"
   local readonly mount_options="${6:-defaults,nofail}"
   local readonly fs_tab_path="/etc/fstab"
+  local device_path="/dev/${device_name}"
 
   # check if the expected device name exists
   # if not, likely is a nitro based instance and need to get the nvmeXn1 device name
-  if [ ! -e $device_name ]; then
-    log_info "Device not found with name '$device_name' looking up by size '${ebs_size}'"
+  if [ ! -e $device_path ]; then
+    log_info "Device not found with name '${device_name}' looking up by size '${ebs_size}'"
     disk_name=`lsblk -do NAME,SIZE | grep ${ebs_size}G | cut -d ' ' -f 1`
     if [[ "$disk_name" == nvme* ]]; then
       ebs_device_name="/dev/${disk_name}"
-      log_info "Update device_name with '$ebs_device_name'"
-      device_name=$ebs_device_name
+      log_info "Update device_path with '${ebs_device_name}'"
+      device_path=$ebs_device_name
     fi
   fi
 
   case "$file_system_type" in
     "ext4")
-      log_info "Creating $file_system_type file system on $device_name..."
-      mkfs.ext4 -F "$device_name"
+      log_info "Creating $file_system_type file system on $device_path..."
+      mkfs.ext4 -F "$device_path"
       ;;
     "xfs")
-      log_info "Creating $file_system_type file system on $device_name..."
-      mkfs.xfs -f "$device_name"
+      log_info "Creating $file_system_type file system on $device_path..."
+      mkfs.xfs -f "$device_path"
       ;;
     *)
       log_error "The file system type '$file_system_type' is not currently supported by this script."
@@ -46,7 +47,7 @@ function mount_volume {
   mkdir "$mount_point"
 
   log_info "Adding device $device_name to $fs_tab_path with mount point $mount_point..."
-  echo "$device_name       $mount_point   $file_system_type    $mount_options  0 2" >> "$fs_tab_path"
+  echo "$device_path       $mount_point   $file_system_type    $mount_options  0 2" >> "$fs_tab_path"
 
   log_info "Mounting volumes..."
   mount -a
