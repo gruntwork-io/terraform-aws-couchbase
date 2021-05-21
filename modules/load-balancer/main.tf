947107bd00c30a36b8135e7329bb38bd0cd88cb8
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2021-05-21 10:03:09 +0100

    Add required_providers block

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -3,6 +3,13 @@ terraform {
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
   # forwards compatible with 0.14.x code.
   required_version = ">= 0.12.26"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
